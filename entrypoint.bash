081fe4727ba7bf414bc6b99c9473235aeeae4229
Michael Gidalevich <mgidalevich@drivenets.com>
2018-10-23 21:31:27 +0300

    test base and dockerfiles separately

--- entrypoint.bash
+++ entrypoint.bash
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-for docker_file in $(find . -name *Dockerfile); do
+for docker_file in $(find . -name baseDockerfile); do
     echo
     echo "Checking ${docker_file} ..."
-    dockerfile_lint -j -f ${docker_file} | jq . 2>&1 | tee output.log
-    [[ $(cat output.log | jq -r "[.error, .warn] | map(select(.count > 0)) | select(length > 0)" | wc -c) -eq 0 ]] || exit 1
+    dockerfile_lint -j -f ${docker_file} -r /opt/dockerfile_lint/config/baseDockerfile_rules.yaml | jq . > output.log
+    RET=$(cat output.log | jq -r "[.error, .warn] | map(select(.count > 0)) | select(length > 0)" | wc -c)
+    if [[ ! ${RET} == 0 ]]; then
+        cat output.log
+        exit 1
+    fi
+done
+
+for docker_file in $(find . -name Dockerfile); do
+    echo
+    echo "Checking ${docker_file} ..."
+    dockerfile_lint -j -f ${docker_file} | jq . > output.log
+    RET=$(cat output.log | jq -r "[.error, .warn] | map(select(.count > 0)) | select(length > 0)" | wc -c)
+    if [[ ! ${RET} == 0 ]]; then
+        cat output.log
+        exit 1
+    fi
 done
